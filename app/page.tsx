d165650204215ea0d39cff748b957cc3ee78ac3b
Danny <29934021+mtlaso@users.noreply.github.com>
2024-11-10 00:00:16 -0500

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { BiCodeCurly } from "react-icons/bi";
 import {
 	FaLocationDot,
@@ -10,15 +11,28 @@ import {
 export default function Home() {
 	return (
 		<main className="min-h-screen px-4">
+			<NavBar />
 			<HeroSection />
 			<p className="text-red-500 sm:text-purple-500 md:text-red-500">test</p>
 		</main>
 	);
 }
 
+const NavBar = () => {
+	return (
+		<nav className="mb-12 py-5">
+			<Link href="/" passHref legacyBehavior>
+				<a href="/" className="font-semibold tracking-tight hover:opacity-90">
+					NomSite
+				</a>
+			</Link>
+		</nav>
+	);
+};
+
 const HeroSection = () => {
 	return (
-		<section className="">
+		<section className="flex flex-col gap-1">
 			<div>
 				<p className="text-4xl">Danny D.</p>
 			</div>
@@ -33,7 +47,7 @@ const HeroSection = () => {
 				<p>QC, CAN 🍁</p>
 			</div>
 
-			<div className="flex items-center gap-2">
+			<div className="flex gap-2">
 				<FaRegEnvelope />
 				<FaLinkedin />
 				<FaSquareGithub />
