8d28680279d732f4f9d94da5d1eae33d7b6af39f
Danny <29934021+mtlaso@users.noreply.github.com>
2024-11-09 23:30:54 -0500

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import { BiCodeCurly } from "react-icons/bi";
+import {
+	FaLocationDot,
+	FaRegEnvelope,
+	FaLinkedin,
+	FaSquareGithub,
+} from "react-icons/fa6";
 
 export default function Home() {
 	return (
-		<main className="">
+		<main className="min-h-screen px-4">
 			<HeroSection />
-			<p className="text-red-500 sm:text-purple-500">test</p>
+			<p className="text-red-500 sm:text-purple-500 md:text-red-500">test</p>
 		</main>
 	);
 }
@@ -12,11 +19,25 @@ export default function Home() {
 const HeroSection = () => {
 	return (
 		<section className="">
-			<p>Danny D.</p>
-			<p>
-				Bonjour 👋. Je suis un développeur web full-stack spécialisé dans le
-				développement d'applications web modernes.
-			</p>
+			<div>
+				<p className="text-4xl">Danny D.</p>
+			</div>
+
+			<div className="flex items-center gap-1">
+				<BiCodeCurly className="inline" />
+				<p>Développeur full stack</p>
+			</div>
+
+			<div className="flex items-center gap-1">
+				<FaLocationDot />
+				<p>QC, CAN 🍁</p>
+			</div>
+
+			<div className="flex items-center gap-2">
+				<FaRegEnvelope />
+				<FaLinkedin />
+				<FaSquareGithub />
+			</div>
 		</section>
 	);
 };
