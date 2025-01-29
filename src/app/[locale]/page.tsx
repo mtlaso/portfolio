fe8e4b6e4b76ee0307a67ca6988edbe853f82577
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-29 03:18:35 -0500

    style: ajout animation

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import { BlobFollowingCursor } from "@/app/[locale]/ui/index/blob-following-curor";
 import { HeroSection } from "@/app/[locale]/ui/index/hero-section/hero-section";
 import { LinksSection } from "@/app/[locale]/ui/index/links-section/links-section";
 import { NavBar } from "@/app/[locale]/ui/index/nav-bar";
@@ -6,6 +7,7 @@ import { WorkSection } from "@/app/[locale]/ui/index/work-section";
 export default function Home() {
 	return (
 		<main className="min-h-screen max-w-3xl mx-auto px-4">
+			<BlobFollowingCursor />
 			<NavBar />
 			<HeroSection />
 			<WorkSection />
---
+++ src/app/[locale]/ui/index/blob-following-curor.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export function BlobFollowingCursor(): React.JSX.Element {
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const onMouseMove = (e: MouseEvent) => {
+			if (ref.current) {
+				ref.current.animate(
+					{
+						left: `${e.clientX}px`,
+						top: `${e.clientY}px`,
+					},
+					{
+						duration: 3000,
+						fill: "forwards",
+					},
+				);
+			}
+		};
+
+		document.addEventListener("mousemove", onMouseMove);
+
+		return () => {
+			document.removeEventListener("mousemove", onMouseMove);
+		};
+	}, []);
+
+	return (
+		<>
+			<div
+				ref={ref}
+				className="fixed top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2
+          rounded-full h-32 -z-10 bg-blend-multiply
+          bg-linear-to-b from-purple-200 to-purple-700 dark:from-purple-700 from-10% dark:to-zinc-900
+          animate-blob aspect-square"
+			/>
+			<div className="fixed inset-0 -z-10 backdrop-blur-md backdrop-hue-rotate-90 dark:hue-rotate-180" />
+		</>
+	);
+}
